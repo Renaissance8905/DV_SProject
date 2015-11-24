014cfa7b5a014ad20e07354568c6b3e68fc097c3
unknown <james_h.thorpe@yahoo.com>
2015-11-23 19:13:58 -0600

    Finished my shiny app.

--- 02 Shiny/James/ui.R
+++ 02 Shiny/James/ui.R
@@ -1,41 +1,55 @@
-#ui.R 
+#ui.R
 
-library(shiny)
+require(shiny)
+require(shinydashboard)
+require(leaflet)
 
-navbarPage(
-  title = "Elements of Visualization",
-  tabPanel(title = "Crosstab",
-     sidebarPanel(
-       actionButton(inputId = "light", label = "Light"),
-       actionButton(inputId = "dark", label = "Dark"),
-       sliderInput("KPI1", "KPI_Low_Max_value:", 
-                   min = 1, max = 4750,  value = 4750),
-       sliderInput("KPI2", "KPI_Medium_Max_value:", 
-                   min = 4750, max = 5000,  value = 5000),
-       textInput(inputId = "title", 
-                 label = "Crosstab Title",
-                 value = "Diamonds Crosstab\nSUM_PRICE, SUM_CARAT, SUM_PRICE / SUM_CARAT"),
-       actionButton(inputId = "clicks1",  label = "Click me")
-     ),
-     
-     mainPanel(plotOutput("distPlot1")
-     )
+dashboardPage(
+  dashboardHeader(
   ),
-  tabPanel(title = "Barchart",
-     sidebarPanel(
-       actionButton(inputId = "clicks2",  label = "Click me")
-     ),
-     
-     mainPanel(plotOutput("distPlot2")
-     )
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("James' Crosstab", tabName = "crosstab", icon = icon("dashboard")),
+      menuItem("Quan's Barchart", tabName = "barchart", icon = icon("th")),
+      menuItem("Blending", tabName = "blending", icon = icon("th")),
+      menuItem("Map", tabName = "map", icon = icon("th")),
+      menuItem("Table", tabName = "table", icon = icon("th"))
+    )
   ),
-  tabPanel(title = "Blending 2 Data Sources",
-     sidebarPanel(
-       actionButton(inputId = "clicks3",  label = "Click me")
-     ),
-     
-     mainPanel(plotOutput("distPlot3")
-     )        
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "crosstab",
+              sliderInput("KPI1", "KPI_Low_Max_value:", 
+                          min = 0, max = 1,  value = 0.8),
+              sliderInput("KPI2", "KPI_Medium_Max_value:", 
+                          min = 1, max = 2,  value = 1.2),
+              textInput(inputId = "title", 
+                        label = "Crosstab Title",
+                        value = "Asylum Preferences by Origin Country"),
+              plotOutput("distPlot1")
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "barchart",
+              plotOutput("distPlot2")
+      ),
+      
+      # Third tab content
+      tabItem(tabName = "blending",
+              actionButton(inputId = "clicks3",  label = "Click me"),
+              plotOutput("distPlot3")
+      ),
+      
+      # Fourth tab content
+      tabItem(tabName = "map",
+              leafletOutput("map")
+      ),
+      
+      # Fifth tab content
+      tabItem(tabName = "table",
+              dataTableOutput("table")
+      )
+    )
   )
 )
-  
