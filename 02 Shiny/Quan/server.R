4a640066133e7d8ab58068e6bdcb020797d02291
Quan Anh Nguyen <quannguyen1604@utexas.edu>
2015-11-23 18:41:41 -0600

    upload my viz

--- 02 Shiny/Quan/server.R
+++ 02 Shiny/Quan/server.R
@@ -3,36 +3,137 @@ require("jsonlite")
 require("RCurl")
 require(ggplot2)
 require(dplyr)
-require(tidyr)
 require(shiny)
+require(shinydashboard)
+require(leaflet)
+require(DT)
 
 shinyServer(function(input, output) {
   
-  output$distPlot <- renderPlot({
-    
-    KPI_Low_Max_value = input$KPI1     
-    KPI_Medium_Max_value = input$KPI2
-    
-    df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-"""select ASYLUM_COUNTRY, RECORD_YEAR, REFUGEES, ORIGIN_COUNTRY from Refugee_Stats;"""')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); 
-    
-    df <- df %>% filter (as.character(ORIGIN_COUNTRY) == "Viet Nam", as.numeric(as.character(REFUGEES)) != "null") %>% group_by(RECORD_YEAR) %>% arrange(desc(as.character(ASYLUM_COUNTRY)))
-    
-    df2 <- df %>% group_by(RECORD_YEAR) %>% summarise(sum_refugees = sum(as.numeric(as.character(REFUGEES))))
-    
-    
-    p1 <- ggplot(df, aes(x=as.character(RECORD_YEAR), y = as.numeric(as.character(REFUGEES)), fill = as.character(ASYLUM_COUNTRY))) + 
+  KPI_Low_Max_value <- reactive({input$KPI1})     
+  KPI_Medium_Max_value <- reactive({input$KPI2})
+  rv <- reactiveValues(alpha = 0.50)
+  observeEvent(input$light, { rv$alpha <- 0.50 })
+  observeEvent(input$dark, { rv$alpha <- 0.75 })
+  
+  df1 <- eventReactive(input$clicks1, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+                                                                                 "select color, clarity, sum_price, round(sum_carat) as sum_carat, kpi as ratio, 
+                                                                                 case
+                                                                                 when kpi < "p1" then \\\'03 Low\\\'
+                                                                                 when kpi < "p2" then \\\'02 Medium\\\'
+                                                                                 else \\\'01 High\\\'
+                                                                                 end kpi
+                                                                                 from (select color, clarity, 
+                                                                                 sum(price) as sum_price, sum(carat) as sum_carat, 
+                                                                                 sum(price) / sum(carat) as kpi
+                                                                                 from diamonds
+                                                                                 group by color, clarity)
+                                                                                 order by clarity;"
+                                                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', 
+                                                                                                   MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value(), p2=KPI_Medium_Max_value()), verbose = TRUE)))
+  })
+  
+  output$distPlot1 <- renderPlot({             
+    plot <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_discrete() +
+      labs(title=isolate(input$title)) +
+      labs(x=paste("COLOR"), y=paste("CLARITY")) +
+      layer(data=df1(), 
+            mapping=aes(x=COLOR, y=CLARITY, label=SUM_PRICE), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="text",
+            geom_params=list(colour="black"), 
+            position=position_identity()
+      ) +
+      layer(data=df1(), 
+            mapping=aes(x=COLOR, y=CLARITY, fill=KPI), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="tile",
+            geom_params=list(alpha=rv$alpha), 
+            position=position_identity()
+      )
+    plot
+  }) 
+  
+  observeEvent(input$clicks, {
+    print(as.numeric(input$clicks))
+  })
+  
+  # Begin code for Second Tab:
+  
+  dfQ <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"""select ASYLUM_COUNTRY, RECORD_YEAR, REFUGEES, ORIGIN_COUNTRY from Refugee_Stats;"""')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+  
+  dfQ <- dfQ %>% filter (as.character(ORIGIN_COUNTRY) == "Viet Nam", as.numeric(as.character(REFUGEES)) != "null") %>% group_by(RECORD_YEAR) %>% arrange(desc(as.character(ASYLUM_COUNTRY)))
+  
+  dfQ2 <- dfQ %>% group_by(RECORD_YEAR) %>% summarise(sum_refugees = sum(as.numeric(as.character(REFUGEES))))
+  
+  output$distPlot2 <- renderPlot(height=600, width=1100, {
+    pQ <- ggplot(dfQ, aes(x=as.character(RECORD_YEAR), y = as.numeric(as.character(REFUGEES)), fill = as.character(ASYLUM_COUNTRY))) + 
       geom_bar(stat = "identity") + 
       labs(title='A Look at Migration Out of Vietnam') + 
       labs(x="Year", y="Refugees")
     
-    p1 <- p1  +
-      geom_text(data = df2, 
+    pQ <- pQ  +
+      geom_text(data = dfQ2, 
                 aes(y = sum_refugees, label = sum_refugees, fill = NULL), size = 4,
                 vjust = -0.5) +
-      geom_hline(data = df2, aes(yintercept = mean(as.numeric(as.character(sum_refugees))))) + 
+      geom_hline(data = dfQ2, aes(yintercept = mean(as.numeric(as.character(sum_refugees))))) + 
       annotate("text", x = 1.5, y = 310000, label = 301027, size = 4)
     
-    return(p1)
+    pQ
+  })
+  
+  # Begin code for Third Tab:
+  
+  df3 <- eventReactive(input$clicks3, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+                                                                                 """select region || \\\' \\\' || \\\'Sales\\\' as measure_names, sum(sales) as measure_values from SUPERSTORE_SALES_ORDERS
+                                                                                 where country_region = \\\'United States of America\\\'
+                                                                                 group by region
+                                                                                 union all
+                                                                                 select market || \\\' \\\' || \\\'Coffee_Sales\\\' as measure_names, sum(coffee_sales) as measure_values from COFFEE_CHAIN
+                                                                                 group by market
+                                                                                 order by 1;"""
+                                                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', 
+                                                                                                   MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+  })
+  
+  output$distPlot3 <- renderPlot(height=1000, width=2000, {
+    plot3 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_continuous() +
+      #facet_wrap(~CLARITY, ncol=1) +
+      labs(title='Blending 2 Data Sources') +
+      labs(x=paste("Region Sales"), y=paste("Sum of Sales")) +
+      layer(data=df3(), 
+            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(colour="blue"), 
+            position=position_identity()
+      ) + coord_flip() +
+      layer(data=df3(), 
+            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES, label=round(MEASURE_VALUES)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="text",
+            geom_params=list(colour="black", hjust=-0.5), 
+            position=position_identity()
+      )
+    plot3
+  })
+  
+  # Begin code for Fourth Tab:
+  output$map <- renderLeaflet({leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 17) %>% addPopups(-93.65, 42.0285, 'Here is the Department of Statistics, ISU')
+  })
+  
+  # Begin code for Fifth Tab:
+  output$table <- renderDataTable({datatable(df1())
   })
 })
